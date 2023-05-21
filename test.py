ecfb6986a32d3aa06d60a18a08b314843807cc37
PlanB <49097278+100trillionUSD@users.noreply.github.com>
2023-05-21 13:19:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ for l in open('data_test.txt','r'):
    v=l.split(" ")
    b,t,i,o=int(v[0]),int(v[1]),int(v[2]),int(v[3])
    for x in range(4+i,4+i+o):
-    vv=float(v[x])
+    vv=int(float(v[x])*100000000)
     if vv>0: d[v[x+o][-16:]]=(b,vv)
    for x in range(4,4+i): d.pop(v[x][-16:], None)
 print(len(d))
