fe801f307cce8189831a9d02b0dad0a9742810e9
PlanB <49097278+100trillionUSD@users.noreply.github.com>
2023-08-08 17:02:27 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-d={}
-for l in open('data_test','r'):
-   v=l.split(" ")
-   b,t,i,o=int(v[0]),int(v[1]),int(v[2]),int(v[3])
-   for x in range(4+i,4+i+o):
-    vv=int(float(v[x])*100000000)
-    if vv>0: d[v[x+o]=(b,vv)
-   for x in range(4,4+i): d.pop(v[x], None)
-print(len(d))
