e6079c89d01a8757c35850d1a7f1b365ec3a41f8
Hussan Choudhry <hussan@uselinkthrow.com>
2023-09-03 21:25:21 +0100

    feat: use map

--- main.go
+++ main.go
@@ -7,25 +7,22 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
-	"sync/atomic"
 	"time"
+
+	"github.com/dolthub/swiss"
 )
 
 func main() {
 	dt := time.Now()
 	fmt.Println("Script started at: ", dt.String())
 
-	var wg sync.WaitGroup
+	a := swiss.NewMap[string, struct{}](42)
 	readFile, err := os.Open("./200k")
 	if err != nil {
 		fmt.Println("Error opening file: ", err)
 		os.Exit(-1)
+
 	}
-
-	var insertions uint64
-	var deletions uint64
-
 	fileScanner := bufio.NewReader(readFile)
 
 	for {
@@ -38,49 +35,37 @@ func main() {
 			os.Exit(-1)
 		}
 
-		wg.Add(1)
-		go func(s string) {
-			defer wg.Done()
+		v := strings.Split(line, " ")
+		i, err := strconv.ParseInt(v[2], 10, 64)
+		if err != nil {
+			fmt.Println("Could not parse int: ", err)
+			os.Exit(-1)
+		}
+		o, err := strconv.ParseInt(v[3], 10, 64)
+		if err != nil {
+			fmt.Println("Could not parse int: ", err)
+			os.Exit(-1)
+		}
 
-			v := strings.Split(line, " ")
-			i, err := strconv.ParseInt(v[2], 10, 64)
+		for x := (4 + i); x < (4 + i + o); x++ {
+			f, err := strconv.ParseFloat(v[x], 64)
 			if err != nil {
-				fmt.Println("Could not parse int: ", err)
+				fmt.Println("Could not parse float: ", err)
 				os.Exit(-1)
 			}
-			o, err := strconv.ParseInt(v[3], 10, 64)
-			if err != nil {
-				fmt.Println("Could not parse int: ", err)
-				os.Exit(-1)
-			}
+			vv := int64(f * 100000000)
 
-			for x := (4 + i); x < (4 + i + o); x++ {
-				f, err := strconv.ParseFloat(v[x], 64)
-				if err != nil {
-					fmt.Println("Could not parse float: ", err)
-					os.Exit(-1)
-				}
-				vv := int64(f * 100000000)
-
-				if vv > 0 {
-					// replacing adding to hashmap here, no need to keep track of the uuids, more performant to just keep track of no
-					atomic.AddUint64(&insertions, 1)
-				}
+			if vv > 0 {
+				m.Put(v[x+o], struct{}{})
 			}
+		}
 
-			for x := int64(4); x < (4 + i); x++ {
-				// replacing map deletion here
-				atomic.AddUint64(&deletions, 1)
-			}
-		}(line)
+		for x := int64(4); x < (4 + i); x++ {
+			m.Delete(v[x])
+		}
 	}
 
-	// waiting for all threads to complete
-	wg.Wait()
-
-	fmt.Println("insertions: ", insertions)
-	fmt.Println("deletions: ", deletions)
-	fmt.Println("answer: ", insertions-deletions)
+	fmt.Println(m.Count())
 
 	dt2 := time.Now()
 
